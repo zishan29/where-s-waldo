604970181b7d6ef21d26962731655d44be3e99aa
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-13 15:40:13 +0530

    Delete unwanted files

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA0HIHcpv2U7SX0obq9TzNZoqzUhJwy02c',
-  authDomain: 'wheres-waldo-106ad.firebaseapp.com',
-  projectId: 'wheres-waldo-106ad',
-  storageBucket: 'wheres-waldo-106ad.appspot.com',
-  messagingSenderId: '797614586355',
-  appId: '1:797614586355:web:096d598c2acd69e8307a86',
-  measurementId: 'G-78D9N9MMK3',
+  apiKey: 'AIzaSyCx4b6BhfRqNzx20hfpuFMFjv9tmhfeGWI',
+  authDomain: 'where-s-waldo-8e291.firebaseapp.com',
+  projectId: 'where-s-waldo-8e291',
+  storageBucket: 'where-s-waldo-8e291.appspot.com',
+  messagingSenderId: '199504262380',
+  appId: '1:199504262380:web:d6027d0860eb3ea200a1c9',
+  measurementId: 'G-XRX53QC2H7',
 };
 
 const app = initializeApp(firebaseConfig);
